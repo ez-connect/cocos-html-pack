9ade1bf820e488e1de595cdd2fb0f0c64240100c
Vinh <thanh.vinh@hotmail.com>
2021-01-02 16:41:28 +0700

    Allow minify

--- src/cocos-html-pack.ts
+++ src/cocos-html-pack.ts
@@ -1,8 +1,14 @@
 import Commander from 'commander';
+import minify from 'minify';
 
 import { Packer, Reader } from './packer';
 
-function main(dir: string, filename: string, verbose?: boolean) {
+function main(
+  dir: string,
+  filename: string,
+  min?: boolean,
+  verbose?: boolean,
+) {
   console.log(dir, filename, verbose);
 
   Reader.setWorkingDir(dir);
@@ -10,15 +16,21 @@ function main(dir: string, filename: string, verbose?: boolean) {
   Packer.load(data);
   const html = Packer.patch();
   Packer.write(filename, html);
+
+  if (min) {
+    minify(filename).then((value) => {
+      Packer.write(filename, value);
+    });
+  }
 }
 
-Commander
-  .description('Single html web mobile template for Cocos')
+Commander.description('Single html web mobile template for Cocos')
   .version('0.0.1')
   .arguments('<dir> <filename>')
   .option('-v --verbose [verbose]', 'output log')
+  .option('-min --minify [minify]', 'Minimize HTML')
   .action((dir, filename) => {
-    main(dir, filename, Commander.verbose);
+    main(dir, filename, Commander.minify, Commander.verbose);
   });
 
 Commander.parse(process.argv);
