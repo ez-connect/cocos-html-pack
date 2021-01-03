8e60e1c9515a71f0211b378225a1bc65781a4f07
Vinh <thanh.vinh@hotmail.com>
2021-01-03 14:34:26 +0700

    Update package name

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# cocos-web-mobile-template
+# Cocos HTML Pack
+
 Single html web mobile template for Cocos
+
+Based on https://github.com/revelatiosgn/SingleHTML5Generator
+
+## Install
+
+As a global package
+
+```
+npm i -g cocos-html-pack
+```
+
+Or a dependency
+
+```
+npm i cocos-html-pack -save-dev
+yarn add -D cocos-html-pack
+```
+
+## Usage
+
+```
+cocos-html-pack [options] <dir> <title> <filename>
+
+Options:
+  -V, --version           output the version number
+  -min --minify [minify]  Minimize HTML
+  -v --verbose [verbose]  output log
+  -h, --help              display help for command
+```
--- src/cocos-html-pack.ts
+++ src/cocos-html-pack.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import Commander from 'commander';
 import minify from 'minify';
 
@@ -10,11 +12,27 @@ function main(
   min?: boolean,
   verbose?: boolean,
 ) {
-  console.log(dir, filename, verbose);
+  if (verbose) {
+    console.log('Input dir:', dir);
+    console.log('Output file:', filename);
+  }
+
+  // Copy template
+  const templateDir = path.join(
+    require.main?.path ?? '',
+    '..',
+    'templates',
+    'web-mobile',
+  );
+  fs.copyFileSync(
+    path.join(templateDir, 'index.html'),
+    path.join(dir, 'index.html'),
+  );
+  fs.copyFileSync(path.join(templateDir, 'main.js'), path.join(dir, 'main.js'));
 
   Reader.setWorkingDir(dir);
   const data = Reader.readAll();
-  Packer.load(data, 'Single HTML');
+  Packer.load(data, title);
   const html = Packer.patch();
   Packer.write(filename, html);
 
@@ -28,8 +46,8 @@ function main(
 Commander.description('Single html web mobile template for Cocos')
   .version('0.0.1')
   .arguments('<dir> <title> <filename>')
-  .option('-v --verbose [verbose]', 'output log')
   .option('-min --minify [minify]', 'Minimize HTML')
+  .option('-v --verbose [verbose]', 'output log')
   .action((dir, title, filename) => {
     main(dir, title, filename, Commander.minify, Commander.verbose);
   });
